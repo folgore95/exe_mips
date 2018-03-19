4d03ff3f0af91bc74876ec90b9c76fe6467d4a02
Giorgio Cantoni <giorgio.canto98@gmail.com>
2018-03-19 10:14:22 +0100

    Update es_3.7.s

--- es_3.7.s
+++ es_3.7.s
@@ -1,10 +1,11 @@
 # Pag 127 
 # Mostrare la singola istruzione MIPS o la sequenza minima d'istruzioni per il seguente costrutto C:
 # x[10]=x[11]+c
+# Si assuma che c sia il registro 13 e che la matrice x inizi alla locazione di memoria 4 000 000
 
 addi $16, $0, 11   # $16 <- 11
 muli $15, $16, 4   # $15=$16*4
-lw $14, 1024($15)   # Passo il $15 dalla memoria ai registri
+lw $14, 4000000($15)   # Passo il $15 dalla memoria ai registri
 add $14, $14, $13   # $14=$14+$13 -> $14= x[11]+c
 subi $15, $15, 4   # Tolgo a $15 4 byte
-sw $14, 1024($15)
+sw $14, 4000000($15)
