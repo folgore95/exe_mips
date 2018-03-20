61fed47d7036c5a7f2323d7b18dd07819a278d68
Giorgio Cantoni <giorgio.canto98@gmail.com>
2018-03-20 10:07:23 +0100

    Create es_7.s

---
+++ var/es_7.s
@@ -0,0 +1,22 @@
+# Testo esercizio preso da http://www.diee.unica.it/~roli/SEI/modulo-calcolatori/slides/esercitazioni/es4.pdf
+
+# Da codice C ad Assembly:
+# int proc(int g, int h, int i, int j) {
+#  int f;
+#  f = (g+h) - (i+j);
+#  return f;
+#  g, h , i, j sono nei registri $4 - $7 ; f venga restituito in $4 ; per allocare g+h -> $9 e i+j -> $10                                    }
+
+# Svolgimento
+proc: addi $29, $29, -8   # Creo spazio sulla pila per i registri // posso usare anche la sottraz. immediata: subi $29, $29, 8
+      sw $10, 0($29)   # Salvo i 2 registri interessati nella pila
+      sw $9, 4($29)
+      
+      add $9, $4, $5   # g+h
+      add $10, $6, $7   # i+j
+      sub $4, $9, $10   # f = (g+h)-(i+j)
+      
+      lw $10, 0($29)   # Ripristino i registri nella pila
+      lw $9, 4($29)
+      addi $29, $29, 8   # Ripristino dello stack della pila
+      jr $31    # Ritorno alla procedura chiamante
